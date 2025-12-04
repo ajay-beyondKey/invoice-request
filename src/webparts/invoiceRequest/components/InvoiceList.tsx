a45e84c8ea67d09832712f4e7a3e6b617fab149d
Ajay Jangam <ajay.jangam@beyondkey.com>
2025-12-04 15:47:35 +0530

    Added dummy data for testing

--- src/webparts/invoiceRequest/components/InvoiceList.tsx
+++ src/webparts/invoiceRequest/components/InvoiceList.tsx
@@ -12,32 +12,32 @@ interface IProps {
 
 // Dummy data for testing
 
-// const itemsDummy: IInvoice[] = [
-//     {
-//         Title: "Test Member",
-//         Email: "test@gmail.com",
-//         CompanyName: "Test Compony",
-//         FirstName: "Test",
-//         LastName: "Member",
-//         StreetAddress: "Test Street, Sample Pune",
-//         StateRegion: "Maharashtra",
-//         CountryRegion: "India",
-//         PostalCode: "411027",
-//         PhoneNumber: "9911223344",
-//         Product: "Another Product",
-//         Price: 50000,
-//         InvoiceDueDate: "2025-11-30",
-//         InvoiceNumber: "23142343",
-//         InvoiceInstructions: "Text sample invoice instruction",
-//     },
-// ];
+const itemsDummy: IInvoice[] = [
+    {
+        Title: "Test Member",
+        Email: "test@gmail.com",
+        CompanyName: "Test Compony",
+        FirstName: "Test",
+        LastName: "Member",
+        StreetAddress: "Test Street, Sample Pune",
+        StateRegion: "Maharashtra",
+        CountryRegion: "India",
+        PostalCode: "411027",
+        PhoneNumber: "9911223344",
+        Product: "Another Product",
+        Price: 50000,
+        InvoiceDueDate: "2025-11-30",
+        InvoiceNumber: "23142343",
+        InvoiceInstructions: "Text sample invoice instruction",
+    },
+];
 
 const InvoiceList: React.FC<IProps> = ({ items, onEdit, onDelete }) => {
     if (!items || items.length === 0) return <div>No invoices yet.</div>;
 
     return (
         <div className={styles.listWrap}>
-            {items.map((it) => (
+            {itemsDummy.map((it) => (
                 <InvoiceItem
                     key={it.Id}
                     item={it}
